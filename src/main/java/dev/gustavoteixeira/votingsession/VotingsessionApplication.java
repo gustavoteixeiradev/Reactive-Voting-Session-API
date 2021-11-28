bd5da10e62eb5400a77f3d2dd8fa53770c5e3333
Gustavo Teixeira <iwhrim@gmail.com>
2021-11-28 19:15:24 -0300

    Initial commit

---
+++ src/main/java/dev/gustavoteixeira/votingsession/VotingsessionApplication.java
@@ -0,0 +1,13 @@
+package dev.gustavoteixeira.votingsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingsessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingsessionApplication.class, args);
+	}
+
+}
